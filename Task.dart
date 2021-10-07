eda6fedf93f9055e917b8108b029a3fb1ef95a86
TouqaHassan <92078112+TouqaHassan@users.noreply.github.com>
2021-10-07 18:15:46 +0200

    Calculator

---
+++ Task.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+void main () {
+  print('Enter Frist Operand');
+  int num = int.parse(stdin.readLineSync()!);
+
+  print('Enter Arithmatic Operator');
+  var operator = stdin.readLineSync()!;
+
+  print('Enter Second Operand');
+  int num1 = int.parse(stdin.readLineSync()!);
+  Calculator operations = Calculator();
+  do {
+    switch (operator) {
+      case "+" :
+        print('$num + $num1 =${operations.addition(num, num1)}');
+        break;
+
+      case "-" :
+        print('$num - $num1 = ${operations.subtraction(num, num1)}');
+        break;
+
+      case "*" :
+        print('$num * $num1 = ${operations.multiplication(num, num1)}');
+        break;
+
+      case "/" :
+        print('$num / $num1 = ${operations.dividing(num, num1)}');
+        break;
+
+      default:
+        print('invalid choice');
+    }
+  }
+  while(num!=0);
+  }
+
+class Calculator{
+  int addition(int num, int num1){
+return num + num1;
+    }
+    int subtraction(int num, int num1) {
+      return num - num1;
+    }
+    int multiplication(int num, int num1){
+    return num*num1;
+    }
+    int dividing(int num, int num1){
+  return (num ~/ num1);
+
+    }
+  }
+
+  
